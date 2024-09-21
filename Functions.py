63f60212bae6f2f41e98086bd4166c01464e4320
HUskyGamezGojo <tcs.montclair@gmail.com>
2024-09-21 15:52:44 +0000

    finish func prac day1

--- Functions.py
+++ Functions.py
@@ -7,3 +7,34 @@ def sub2(x,y):
     return x-y
 
 print(sub2(7,3))
+
+
+def multi4(x):
+    return 4*x
+    
+print(multi4(5))
+
+
+def squared(x):
+    return x*x
+    
+print(squared(10))
+
+
+
+def recp(l,w):
+    return l+l+w+w
+
+print(recp(7,3))
+
+
+
+def reca(l,w):
+    return l*w
+
+print(reca(7,3))
+
+def square(l):
+    return l*l+l*4
+
+print(square(5))
