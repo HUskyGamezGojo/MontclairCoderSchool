9e8f52c2164ca2a63bd38e634a2dfcffb998c3b3
HUskyGamezGojo <tcs.montclair@gmail.com>
2024-09-28 15:37:49 +0000

    day2 practice

--- Functions.py
+++ Functions.py
@@ -1,40 +1,60 @@
-def add2(x,y):
-    return x+y
+# def add2(x,y):
+#     return x+y
 
-print(add2(5,6))
+# print(add2(5,6))
 
-def sub2(x,y):
-    return x-y
+# def sub2(x,y):
+#     return x-y
 
-print(sub2(7,3))
+# print(sub2(7,3))
 
 
-def multi4(x):
-    return 4*x
+# def multi4(x):
+#     return 4*x
     
-print(multi4(5))
+# print(multi4(5))
 
 
-def squared(x):
-    return x*x
+# def squared(x):
+#     return x*x
     
-print(squared(10))
+# print(squared(10))
 
 
 
-def recp(l,w):
-    return l+l+w+w
+# def recp(l,w):
+#     return l+l+w+w
 
-print(recp(7,3))
+# print(recp(7,3))
 
 
 
-def reca(l,w):
-    return l*w
+# def reca(l,w):
+#     return l*w
 
-print(reca(7,3))
+# print(reca(7,3))
 
-def square(l):
-    return l*l+l*4
+# def square(l):
+#     return l*l+l*4
 
-print(square(5))
+# print(square(5))
+# def three_add(a,b,c):
+#     return a+b+c
+
+# print (three_add(4,5,6))
+
+# def op(x,y):
+#     l = x+y
+#     w = x*y
+#     return w+l
+# print (op(2,3))
+
+
+# def op2(x,y):
+#     l = x+y
+#     w = x*y
+#     if l>w:
+#      return l-w
+#     else:
+#         return w-l
+# print (op2(6,10))
